25f5cc62fbfb620984cf1e0098ca0a4cd3036dda
Maracash41 <9531945@gmail.com>
2024-01-08 15:30:18 +0300

    refactor(extended-1.2): add media alternatives

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete-btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt = 'remove button icon';
     deleteButton.appendChild(deleteButtonImg);
 
 
